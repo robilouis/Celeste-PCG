ff5dc67baad4720ae2612e6ca8889087480015cf
Louis Robinet <s3003043@vuw.leidenuniv.nl>
2022-10-12 13:08:32 +0200

    First commit - organization of the code

---
+++ playability/playability.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def playability_assessment(room_data):
+    """
+    Assesses if a room is playable. That is, determines if a room can be cleared by a player.
+    Input: room data (format to precise later)
+    Output: boolean, True if room clearable, False if not
+    """
+
+    return True
