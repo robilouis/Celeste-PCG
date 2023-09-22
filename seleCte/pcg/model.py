dd84f143263aaf71b792ee88355a113c3206b9fd
Louis <louis.robinet@alcemy.tech>
2023-09-22 19:09:02 +0200

    updated MCModel class - fitness evaluation

--- seleCte/pcg/model.py
+++ seleCte/pcg/model.py
@@ -5,3 +5,18 @@ class MCModel:
             [0, 1, 1],
             [0, 1, 2],
         ]
+
+def fitness_evaluation(candidate):
+    """
+    Read the name
+    """
+    # penalizes if no or too many exits (BUT can be holes tho, needs to specify this)
+    score_exits = 1000 * get_nb_exits(candidate) * (4 - get_nb_exits(candidate))
+
+    return score_exits
+
+def get_nb_exits(candidate):
+    """
+    Same
+    """
+    raise NotImplementedError
